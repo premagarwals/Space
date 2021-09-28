8467be4290924c3cd90d38dbc3bcdaab165b6bc5
premagarwals <85096702+premagarwals@users.noreply.github.com>
2021-09-28 21:18:30 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ function DisplayHint(code){
     
  if (code === 303.5){
     //Earth and Moon
-    setTimeout(function(){ document.getElementById("moon1").style.left = "0%"; }, 4000);
-    setTimeout(function(){ document.getElementById("moon1").style.left = "-100%"; }, 9000);    
-    setTimeout(function(){ document.getElementById("moon2").style.left = "0%"; }, 10000);
-    setTimeout(function(){ document.getElementById("moon2").style.left = "-100%"; }, 15000);
+    setTimeout(function(){ document.getElementById("moon1").style.left = "5vw"; }, 4000);
+    setTimeout(function(){ document.getElementById("moon1").style.left = "-120vw"; }, 9000);    
+    setTimeout(function(){ document.getElementById("moon2").style.left = "5vw"; }, 10000);
+    setTimeout(function(){ document.getElementById("moon2").style.left = "-120vw"; }, 15000);
  }
  
  
@@ -15,14 +15,14 @@ function DisplayHint(code){
  
  else if (code === 304){
     //Earth and Mars
-    setTimeout(function(){ document.getElementById("mars1").style.left = "0%"; }, 1000);
-    setTimeout(function(){ document.getElementById("mars1").style.left = "-100%"; }, 8000);    
-    setTimeout(function(){ document.getElementById("mars2").style.left = "0%"; }, 10000);
-    setTimeout(function(){ document.getElementById("mars2").style.left = "-100%"; }, 17000);
-    setTimeout(function(){ document.getElementById("mars3").style.left = "0%"; }, 19000);
-    setTimeout(function(){ document.getElementById("mars3").style.left = "-100%"; }, 26000);
-    setTimeout(function(){ document.getElementById("mars4").style.left = "0%"; }, 28000);
-    setTimeout(function(){ document.getElementById("mars4").style.left = "-100%"; }, 35000);
+    setTimeout(function(){ document.getElementById("mars1").style.left = "5vw"; }, 1000);
+    setTimeout(function(){ document.getElementById("mars1").style.left = "-120vw"; }, 8000);    
+    setTimeout(function(){ document.getElementById("mars2").style.left = "5vw"; }, 10000);
+    setTimeout(function(){ document.getElementById("mars2").style.left = "-120vw"; }, 17000);
+    setTimeout(function(){ document.getElementById("mars3").style.left = "5vw"; }, 19000);
+    setTimeout(function(){ document.getElementById("mars3").style.left = "-120vw"; }, 26000);
+    setTimeout(function(){ document.getElementById("mars4").style.left = "5vw"; }, 28000);
+    setTimeout(function(){ document.getElementById("mars4").style.left = "-120vw"; }, 35000);
  }
  
  
