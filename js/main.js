736ac3e27f64ccb3bcb8a68d7bbb11183c204039
premagarwals <85096702+premagarwals@users.noreply.github.com>
2021-09-29 00:11:38 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ function DisplayHint(code){
     
  if (code === 303.5){
     //Earth and Moon
+    setTimeout(function(){ document.getElementById("ufo").style.opacity = "1"; },500);
     setTimeout(function(){ document.getElementById("moon1").style.left = "5vw"; }, 4000);
     setTimeout(function(){ document.getElementById("moon1").style.left = "-120vw"; }, 9000);    
     setTimeout(function(){ document.getElementById("moon2").style.left = "5vw"; }, 10000);
     setTimeout(function(){ document.getElementById("moon2").style.left = "-120vw"; }, 15000);
+    setTimeout(function(){ document.getElementById("ufo").style.opacity = "0"; },18500);
  }
  
  
@@ -15,6 +17,7 @@ function DisplayHint(code){
  
  else if (code === 304){
     //Earth and Mars
+    setTimeout(function(){ document.getElementById("ufo").style.opacity = "1"; },0);
     setTimeout(function(){ document.getElementById("mars1").style.left = "5vw"; }, 1000);
     setTimeout(function(){ document.getElementById("mars1").style.left = "-120vw"; }, 8000);    
     setTimeout(function(){ document.getElementById("mars2").style.left = "5vw"; }, 10000);
@@ -23,6 +26,7 @@ function DisplayHint(code){
     setTimeout(function(){ document.getElementById("mars3").style.left = "-120vw"; }, 26000);
     setTimeout(function(){ document.getElementById("mars4").style.left = "5vw"; }, 28000);
     setTimeout(function(){ document.getElementById("mars4").style.left = "-120vw"; }, 35000);
+    setTimeout(function(){ document.getElementById("ufo").style.opacity = "0"; },36500);
  }
  
  
